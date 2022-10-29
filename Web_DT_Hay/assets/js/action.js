6c816fdd075b97edb374917213f0eaeffc3a3d5e
Anh Quang <anhquang2009@gmail.com>
2022-10-29 18:56:39 +0700

    dâsd

--- Web_DT_Hay/assets/js/action.js
+++ Web_DT_Hay/assets/js/action.js
@@ -1,34 +1,9 @@
-
-console.log('start');
-var swiper = new Swiper(".slide-show .mySwiper", {
-  spaceBetween: 10,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  thumbs: {
-    swiper:thumbs
-  }
+$(document).ready(function(){
+  $('.slide-show-item').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    arrows:false,
+  });
 });
-
-var thumbs = new Swiper(".thumbs .mySwiper2", {
-  spaceBetween: 10,
-  slidesPerView: 5,
-  loop:false,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-   centeredSlides: true,
-  centeredSlidesBounds: true,
-});
-// SẢN PHẦM BÁN CHẠY
-// var swiper1 = new Swiper(".mySwiper3", {
-//   slidesPerView: 5,
-//   loop:true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-  
-// });
-// console.log('end');
 
