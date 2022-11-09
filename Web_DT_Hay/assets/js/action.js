b7ffd894dc6c5a76e44d276fd7e06e1f20af179d
Anh Quang <anhquang2009@gmail.com>
2022-11-09 23:23:24 +0700

    aa

--- Web_DT_Hay/assets/js/action.js
+++ Web_DT_Hay/assets/js/action.js
@@ -1,36 +1,74 @@
-$(document).ready(function(){
-  $('.slide-show-item').slick({
-    dots: true,
-    speed: 300,
-    slidesToShow: 1,
-    arrows:false,
-    asNavFor: '.thumbs',
-  });
-});
-$(document).ready(function(){
-  $('.thumbs').slick({
-    dots: false,
-    speed: 300,
-    Swipe: true,
-    slidesToShow: 5,
-    arrows:false,
-    infinite:false,
-    asNavFor: '.slide-show-item',
-    focusOnSelect: true,
-  });
-});
-$('.thumbs .slick-slide').removeClass('slick-active');
-//set active class to first thumbnail slides
-$('.thumbs .slick-slide').eq(0).addClass('slick-active');
+// slide
+$(document).ready(function () {
+  $(function () {
+    var $parent = $(".slide-show-item");
+    $check = $parent.attr("data-item");
+    // console.log($parent);
+    // console.log($check);
 
-$(document).ready(function(){
-  $('.post2 .bot-block .tab-content .product-tab .slide-show-list').slick({
+    if ($check == 5) {
+      $check = true;
+    } else {
+      if ($check > 5) {
+        $check = false;
+      } else {
+        $check = true;
+      }
+    }
+    console.log($check);
+    if ($check) {
+      var $chil = $parent.children().clone();
+      $parent.append($chil);
+    }
+    $('.slide-show-item').slick({
+      dots: true,
+      speed: 300,
+      slidesToShow: 5,
+      slidesToScroll: 5,
+      arrows:false,
+      // asNavFor: '.thumbs',
+    });
+    // console.log($check);
+  });
+
+ 
+  // var $thumbs = $('.thumbs').attr('data-item');
+  // console.log($thumbs);
+
+  // if($thumbs <= 5){
+  //   $thumbs = false;
+  // }
+  // else{
+  //   $thumbs = true;
+  // }
+
+  // console.log($thumbs);
+
+  // $('.thumbs').slick({
+  //   dots: false,
+  //   speed: 300,
+  //   slidesToShow: 5,
+  //   slidesToScroll: 5,
+  //   arrows:false,
+  //   infinite: $thumbs,
+  //   asNavFor: '.slide-show-item',
+  //   // focusOnSelect: true,
+  //   // Swipe: true,
+  // });
+  // $('.thumbs .slick-slide').removeClass('slick-active');
+  // //set active class to first thumbnail slides
+  // $('.thumbs .slick-slide').addClass('slick-active');
+  // end slide
+});
+
+$(document).ready(function () {
+  $(".post2 .slide-show-list").slick({
     slidesToShow: 5,
-    slidesToScroll: 5
+    slidesToScroll: 5,
   });
 });
-$(document).ready(function(){
-  $('.wrapper-content-page .bot-block .tab-content .product-tab .slide-show-list').slick({
+$(document).ready(function () {
+  $(".wrapper-content-page .bot-block .tab-content .product-tab .slide-show-list").slick({
     infinite: false,
     speed: 300,
     slidesToShow: 4,
@@ -38,16 +76,13 @@ $(document).ready(function(){
   });
 });
 // reamle
-$(document).ready(function(){
-  $('.pav-slideShow').slick({
-    arrows:true,
+$(document).ready(function () {
+  $(".pav-slideShow").slick({
+    arrows: true,
     dots: false,
     speed: 1000,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed:1000,
+    autoplaySpeed: 1000,
   });
 });
-
-
-
---
+++ Web_DT_Hay/assets/js/slide.js
@@ -0,0 +1,48 @@
+//slide show
+$(document).ready(function () {
+  $(".slide-show-item").slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: ".thumbs",
+  });
+  var $thumbs = $(".thumbs").attr("data-item");
+  if ($thumbs <= 5) {
+    $thumbs = false;
+  } else {
+    $thumbs = true;
+  }
+  $(".thumbs").slick({
+    dots: false,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    arrows: false,
+    infinite: $thumbs,
+    asNavFor: ".slide-show-item",
+    focusOnSelect: true,
+  });
+  // product
+    $(".post2 .slide-show-list").slick({
+      slidesToShow: 5,
+      slidesToScroll: 5,
+    });
+    $(".wrapper-content-page .bot-block .tab-content .product-tab .slide-show-list").slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+    });
+  //end product
+  // slide realme
+  $(".pav-slideShow").slick({
+    arrows: true,
+    dots: false,
+    speed: 1000,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+});
